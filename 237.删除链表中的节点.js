4034f640cbf575734f58d81b7b4c478ff631bc2f
OddChen <1320199223@qq.com>
2020-09-16 21:36:03 +0800

    删除链表中的一个值

---
+++ 237.删除链表中的节点.js
@@ -0,0 +1,19 @@
+/**
+ * 编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ * 4->5->1->9
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
