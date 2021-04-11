0f458d2a7174633d09c48156df28c84a5d10a4eb
OddChen <1320199223@qq.com>
2021-04-11 22:26:00 +0800

    打卡

---
+++ 264.丑数II.js
@@ -0,0 +1,30 @@
+/**
+ * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * @param {number} n
+ * @return {number}
+ */
+//官方解法，动态规划三指针
+var nthUglyNumber = function (n) {
+  const dp = new Array(n + 1).fill(0)
+  dp[1] = 1
+  let p2 = 1
+  let p3 = 1
+  let p5 = 1
+  for (let i = 2; i <= n; i++) {
+    const num2 = dp[p2] * 2
+    const num3 = dp[p3] * 3
+    const num5 = dp[p5] * 5
+    dp[i] = Math.min(Math.min(num2, num3), num5)
+    if (dp[i] === num2) {
+      p2++
+    }
+    if (dp[i] === num3) {
+      p3++
+    }
+    if (dp[i] === num5) {
+      p5++
+    }
+  }
+  return dp[n]
+}
